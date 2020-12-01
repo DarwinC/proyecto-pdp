29c460606715423af76c87d49b99404f0f1469be
DarwinC <darwin.carrizo@gmail.com>
2020-12-01 12:11:11 -0300

    2 files modified

--- src/app/lista-estacionamientos/lista-estacionamientos.component.ts
+++ src/app/lista-estacionamientos/lista-estacionamientos.component.ts
@@ -24,6 +24,7 @@ export class ListaEstacionamientosComponent implements OnInit {
     ) {
       this.router.navigate([""]);
     }
+    this.obtenerEstacionamientos();
   }
 
   obtenerEstacionamientos() {
