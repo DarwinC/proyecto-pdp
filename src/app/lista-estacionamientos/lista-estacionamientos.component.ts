475ae8dd87f18cbc788c91198b6ea1864a510091
DarwinC <darwin.carrizo@gmail.com>
2020-12-01 11:59:46 -0300

    1 file modified

--- src/app/lista-estacionamientos/lista-estacionamientos.component.ts
+++ src/app/lista-estacionamientos/lista-estacionamientos.component.ts
@@ -1,25 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { UsuarioService } from "../services/usuario.service";
-import { MatriculasService } from "../services/matriculas.service";
+import { EstacionamientosService } from "../services/estacionamientos.service";
 
 @Component({
-  selector: 'app-lista-estacionamientos',
-  templateUrl: './lista-estacionamientos.component.html',
-  styleUrls: ['./lista-estacionamientos.component.css']
+  selector: "app-lista-estacionamientos",
+  templateUrl: "./lista-estacionamientos.component.html",
+  styleUrls: ["./lista-estacionamientos.component.css"]
 })
 export class ListaEstacionamientosComponent implements OnInit {
+  estacionamientos;
 
-    constructor(
-    private matriculasService: MatriculasService,
+  constructor(
+    private estacionamientosService: EstacionamientosService,
     private router: Router,
     private usuariosService: UsuarioService
   ) {}
 
   ngOnInit() {
-        if (!this.usuariosService.isLoggedIn()&&this.usuariosService.isAdminUser()) {
+    if (
+      !this.usuariosService.isLoggedIn() &&
+      this.usuariosService.isAdminUser()
+    ) {
       this.router.navigate([""]);
     }
   }
 
+  obtenerEstacionamientos() {
+    this.estacionamientosService.getAll().subscribe(
+      a => {
+        console.log(a);
+        this.estacionamientos = a;
+        console.log("Se consulto el listado de matriculas");
+      },
+      err => {
+        if (err.status === 500) {
+          console.log("Ha ocurrido un error en el servidor");
+        }
+      }
+    );
+  }
+/*
+  eliminar(idestacionamiento) {
+
+    this.estacionamientosService.remove(idestacionamiento).subscribe(
+      a => {
+        this.obtenerEstacionamientos();
+      },
+      err => {
+        console.log("Ha ocurrido un error");
+        console.log(err);
+      }
+    );
+  }
+  */
+/*
+  modificar(idestacionamiento) {
+    this.estacionamientosService.setEstacionamientoId(idestacionamiento);
+    this.router.navigate(["matriculas/modificar/"]);
+  }
+*/
+  obtenerUsuario(idusuario) {
+    let email = "";
+    console.log("El usuario a buscar" + idusuario);
+    const usuario_tmp = this.usuariosService.getUsuarioById(idusuario);
+
+    console.log(usuario_tmp);
+
+    if (usuario_tmp["usr"] != null) {
+      console.log("Encuentra el usuario" + usuario_tmp["usr"]);
+      console.log(usuario_tmp["usr"]["email"]);
+      email = usuario_tmp["usr"]["email"];
+    }
+    return email;
+  }
 }
